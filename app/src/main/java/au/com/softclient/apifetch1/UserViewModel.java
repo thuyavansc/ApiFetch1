f83d8b3f44e05b04b670d88459cf34011054f7fe
Kingston <Kingston@DESKTOP-MVP19NT>
2023-10-15 22:09:18 +0530

    Not Updating Automatically Mutable LiveData api fetch working working

--- app/src/main/java/au/com/softclient/apifetch1/UserViewModel.java
+++ app/src/main/java/au/com/softclient/apifetch1/UserViewModel.java
@@ -2,6 +2,7 @@ package au.com.softclient.apifetch1;
 
 import android.app.Application;
 import android.os.AsyncTask;
+import android.util.Log; // Import Log class
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
@@ -10,10 +11,12 @@ import okhttp3.Request;
 import okhttp3.Response;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.io.IOException;
 
 public class UserViewModel extends AndroidViewModel {
     private MutableLiveData<User> userLiveData;
+    private static final String TAG = "UserViewModel"; // Define a TAG for logging
 
     public UserViewModel(Application application) {
         super(application);
@@ -25,20 +28,24 @@ public class UserViewModel extends AndroidViewModel {
         return userLiveData;
     }
 
-    private void fetchUser() {
+    public void fetchUser() {
         new FetchUserTask().execute();
     }
 
     private class FetchUserTask extends AsyncTask<Void, Void, User> {
         @Override
         protected User doInBackground(Void... voids) {
-            OkHttpClient client = new OkHttpClient();
-            Request request = new Request.Builder()
-                    .url("http://192.168.8.168:3000/api/users/3") // Replace with your API endpoint
-                    .build();
-
             try {
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url("http://192.168.8.168:3000/api/users/3") // Replace with your API endpoint
+                        .build();
+
+                // Log the API URL for debugging
+                Log.d("TAG", "API URL: " + request.url().toString());
+
                 Response response = client.newCall(request).execute();
+
                 if (response.isSuccessful()) {
                     String jsonData = response.body().string();
                     JSONObject jsonObject = new JSONObject(jsonData);
@@ -47,6 +54,10 @@ public class UserViewModel extends AndroidViewModel {
                     User user = new User();
                     user.setName(userObject.getString("name"));
                     user.setEmail(userObject.getString("email"));
+
+                    // Log the fetched name and email for debugging
+                    Log.d("TAG", "Name: " + user.getName());
+                    Log.d("TAG", "Email: " + user.getEmail());
 
                     return user;
                 }
